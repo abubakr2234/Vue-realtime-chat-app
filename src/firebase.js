f4a1aaaff7f8b7ae96e25303918fe8a52c9e863e
abubakr2234 <68422152+abubakr2234@users.noreply.github.com>
2021-03-19 08:48:00 +0200

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,62 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import Filter from 'bad-words'
+import { ref, onUnmounted, computed } from 'vue'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCOSwFH3IowkQIApB_-7Qy1YPnOUDukW9w",
+    authDomain: "my-vue-chat-80f7a.firebaseapp.com",
+    projectId: "my-vue-chat-80f7a",
+    storageBucket: "my-vue-chat-80f7a.appspot.com",
+    messagingSenderId: "611066534248",
+    appId: "1:611066534248:web:3b71497aa44231ee3c9edd"
+})
+
+const auth = firebase.auth()
+
+export function useAuth() {
+  const user = ref(null)
+  const unsubscribe = auth.onAuthStateChanged(_user => (user.value = _user))
+  onUnmounted(unsubscribe)
+  const isLogin = computed(() => user.value !== null)
+
+  const signIn = async () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider()
+    await auth.signInWithPopup(googleProvider)
+  }
+  const signOut = () => auth.signOut()
+
+  return { user, isLogin, signIn, signOut }
+}
+
+const firestore = firebase.firestore()
+const messagesCollection = firestore.collection('messages')
+const messagesQuery = messagesCollection.orderBy('createdAt', 'desc').limit(100)
+const filter = new Filter()
+
+export function useChat() {
+  const messages = ref([])
+  const unsubscribe = messagesQuery.onSnapshot(snapshot => {
+    messages.value = snapshot.docs
+      .map(doc => ({ id: doc.id, ...doc.data() }))
+      .reverse()
+  })
+  onUnmounted(unsubscribe)
+
+  const { user, isLogin } = useAuth()
+  const sendMessage = text => {
+    if (!isLogin.value) return
+    const { photoURL, uid, displayName } = user.value
+    messagesCollection.add({
+      userName: displayName,
+      userId: uid,
+      userPhotoURL: photoURL,
+      text: filter.clean(text),
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+  }
+
+  return { messages, sendMessage }
+}
